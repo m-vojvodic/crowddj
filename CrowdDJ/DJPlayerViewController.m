f84fefe200dc3c5341ab0cbb1efad8074deffde4
iamcoreyg <iamcoreyg@gmail.com>
2014-10-04 06:41:38 -0700

    merging firebase removal to request-btn branch

--- CrowdDJ/DJPlayerViewController.m
+++ CrowdDJ/DJPlayerViewController.m
@@ -15,19 +15,63 @@
 
 @implementation DJPlayerViewController
 
+// TODO: ping server every 30 seconds
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-
+    // set up table view cells
     [trackQueueTableView registerNib:[UINib nibWithNibName:@"TrackCell" bundle:nil] forCellReuseIdentifier:[TrackCell reuseIdentifier]];
     [trackQueueTableView setRowHeight:64];
     
     tracks = [[NSMutableArray alloc] init];
     
+    // just for testing
+    /*
+    [tracks addObject:@{
+        @"title": @"Munching at Tiannas house",
+        @"uri": @"http://api.soundcloud.com/tracks/13158665",
+        @"permalink_url": @"http://soundcloud.com/user2835985/munching-at-tiannas-house",
+        @"user": @{
+            @"id": @3699101,
+            @"permalink": @"user2835985",
+            @"username": @"user2835985",
+            @"uri": @"http://api.soundcloud.com/users/3699101",
+            @"permalink_url": @"http://soundcloud.com/user2835985",
+            @"avatar_url": @"http://a1.sndcdn.com/images/default_avatar_large.png?142a848"
+        },
+        @"stream_url": @"http://api.soundcloud.com/tracks/13158665/stream",
+        @"download_url": @"http://api.soundcloud.com/tracks/13158665/download"
+    }
+     ];
+    [tracks addObject:@{
+                        @"title": @"Berghain bangers",
+                        @"uri": @"http://api.soundcloud.com/tracks/69422508",
+                        @"permalink_url": @"http://soundcloud.com/jochempaap/speedy-j-berghain-nov-2012",
+                        @"user": @{
+                                @"id": @3699101,
+                                @"permalink": @"user2835985",
+                                @"username": @"berghain",
+                                @"uri": @"http://api.soundcloud.com/users/3699101",
+                                @"permalink_url": @"http://soundcloud.com/user2835985",
+                                @"avatar_url": @"http://a1.sndcdn.com/images/default_avatar_large.png?142a848"
+                                },
+                        @"stream_url": @"http://api.soundcloud.com/tracks/13158665/stream",
+                        @"download_url": @"http://api.soundcloud.com/tracks/13158665/download"
+                        }
+     ];
+    */
+    
+    // TODO: colors / images
+    
     skipButton.titleLabel.text = @"Skip";
     idLabel.text = djId;
 
     [self loadQueue];
+    
+    // load player
+    [self setUrlToPlay];
+    [playerWebView loadHTMLString:urlToPlay baseURL:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -82,12 +126,31 @@
 
 - (void) loadQueue
 {
+    // TODO: send request to server
     [trackQueueTableView reloadData];
+}
+
+// TODO: fix autoplay
+- (void) setUrlToPlay
+{
+    // construct url from queue
+    NSString * beginningOfUrlString = @"<iframe width=\"100%\" height=\"120\" scrolling=\"no\" frameborder=\"no\" src=\"https://w.soundcloud.com/player/?url=";
+    NSString * newUrlToPlay = [[tracks firstObject] objectForKey:@"uri"];
+    NSString * endOfUrlString = @"&auto_play=true\"></iframe>";
+
+    urlToPlay = (NSMutableString *)[[[NSMutableString stringWithString:beginningOfUrlString] stringByAppendingString:newUrlToPlay] stringByAppendingString:endOfUrlString];
 }
 
 -(IBAction) skipSong
 {
-    // TODO:
+    // remove current song, set next song to play, reload table data
+    if([tracks count] > 1){
+        NSLog(@"Skipping song");
+        [tracks removeObjectAtIndex:0];
+        [self setUrlToPlay];
+        [playerWebView loadHTMLString:urlToPlay baseURL:nil];
+        [trackQueueTableView reloadData];
+    }
 }
 
 @end
--- Podfile
+++ Podfile
@@ -1,6 +1,6 @@
 source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '8.0'
 
-pod 'Firebase', '>= 1.2.0'
+pod 'AFNetworking', '~> 2.0'
 pod 'CocoaSoundCloudAPI', '1.0.1'
 pod 'CocoaSoundCloudUI', '1.0.5'
